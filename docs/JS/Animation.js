9f74f6d4c08d4f96482f2ff58ca796695213c9ef
Taku <fk.53rk2@icloud.com>
2026-05-01 11:40:48 +0900

    四角形をクリックすると図形が回転し、数字がカウントされるようにJavaScriptを書いた

--- docs/JS/Animation.js
+++ docs/JS/Animation.js
@@ -1,9 +1,57 @@
 const object = document.getElementById("object");
+const num1 = document.getElementById("Num1")
+// const btn1 = document.getElementById("btn1")
+// const btn2 = document.getElementById("btn2")
 let movingObject = false;
-console.log(movingObject);
+let movingNum1 = false;
+
+
+console.log(movingNum1)
 object.addEventListener("click", () => {
     movingObject = !movingObject;
-    console.log(movingObject);
     movingObject ? object.classList.add("is-active") :
         object.classList.remove("is-active");
+
+    movingNum1 = !movingNum1
+    console.log(movingNum1)
+
+    if (movingNum1) {
+        countUp()
+        num1.classList.add("is-activenum1");
+    } else {
+        clearInterval(intervalId);
+        num1.classList.remove("is-activenum1");
+    }
 });
+
+
+
+let count = 1;
+let intervalId;
+let isRunning = false;
+
+
+const countUp = () => {
+    const text = num1.textContent = count;
+    count++
+    intervalId = setTimeout(countUp, 1000);
+    if (count > 4) {
+        count = 1;
+    }
+};
+
+
+// btn1.addEventListener("click", () => {
+//     if (!isRunning) {
+//         isRunning = true;
+//         countUp();
+//     }
+// });
+
+// btn2.addEventListener("click", () => {
+//     clearTimeout(intervalId);
+//     isRunning = false;
+// })
+
+
+
