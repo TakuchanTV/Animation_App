7e9a30234d90e1f8a5a184bf86d7a853279450d2
Taku <fk.53rk2@icloud.com>
2026-05-01 15:16:24 +0900

    addEventListenerの種類を増やし、クリックしたときとキー入力したときで処理を分けた。モバイルでは画面に触っている状態で指が動いたらイベントが発火するようにした

--- docs/JS/Animation.js
+++ docs/JS/Animation.js
@@ -5,6 +5,8 @@ const num1 = document.getElementById("Num1")
 let movingObject = false;
 let movingNum1 = false;
 
+let rev_movingObject = false;
+let rev_movingNum1 = false;
 
 console.log(movingNum1)
 object.addEventListener("click", () => {
@@ -23,6 +25,61 @@ object.addEventListener("click", () => {
         num1.classList.remove("is-activenum1");
     }
 });
+
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 32) {
+        rev_movingObject = !rev_movingNum1;
+        rev_movingObject ? object.classList.add("rev_is-active") :
+            object.classList.remove("rev_is-active");
+
+        rev_movingNum1 = !rev_movingNum1
+        console.log(movingNum1)
+
+        if (rev_movingNum1) {
+            countUp()
+            num1.classList.add("rev_is-activenum1");
+        } else {
+            clearInterval(intervalId);
+            num1.classList.remove("rev_is-activenum1");
+        }
+    }
+});
+
+document.addEventListener("touchmove", (e) => {
+    if (e.keyCode === 32) {
+        rev_movingObject = !rev_movingNum1;
+        rev_movingObject ? object.classList.add("rev_is-active") :
+            object.classList.remove("rev_is-active");
+
+        rev_movingNum1 = !rev_movingNum1
+        console.log(movingNum1)
+
+        if (rev_movingNum1) {
+            countUp()
+            num1.classList.add("rev_is-activenum1");
+        } else {
+            clearInterval(intervalId);
+            num1.classList.remove("rev_is-activenum1");
+        }
+    }
+});
+
+// object.addEventListener("scroll", () => {
+//     rev_movingObject = !rev_movingNum1;
+//     rev_movingNum1 ? object.classList.add("rev_is-active") :
+//         object.classList.remove("rev_is-active");
+
+//     rev_movingNum1 = !rev_movingNum1
+//     console.log(movingNum1)
+
+//     if (rev_movingNum1) {
+//         countUp()
+//         num1.classList.add("rev_is-activenum1");
+//     } else {
+//         clearInterval(intervalId);
+//         num1.classList.remove("rev_is-activenum1");
+//     }
+// });
 
 
 
